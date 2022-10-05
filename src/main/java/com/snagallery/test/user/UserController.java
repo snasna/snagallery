db90d7ccd99bcbe2fb434ae10c149286b3ae969e
LAPTOP-QSBIPBFB\김재윤 <kimjy9698@naver.com>
2022-10-05 18:48:07 +0900

    2022-10-05 김재윤
    갤러리 홈페이지 1일차

--- src/main/java/com/snagallery/test/user/UserController.java
+++ src/main/java/com/snagallery/test/user/UserController.java
@@ -1,5 +1,24 @@
 package com.snagallery.test.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
+	// 로그인 화면
+	@GetMapping("/signin/view")
+	public String signinView() {
+		return "user/signin";
+	}
+	
+	// 회원가입 화면
+		@GetMapping("/signup/view")
+		public String signupView() {
+			return "user/signup";
+	}
+	
+		
 }
